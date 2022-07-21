d9ba88dc9c50de1a3c39d93dc4519443d0c24d92
xuantrungnd2001 <xuantrungnd2001@gmail.com>
2022-07-21 09:36:29 +0700

    remove ignore

---
+++ vendor/irazasyed/telegram-bot-sdk/src/Objects/ChatMemberUpdated.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Telegram\Bot\Objects;
+
+/**
+ * Class ChatMemberUpdated.
+ *
+ * @link https://core.telegram.org/bots/api#message
+ *
+ * @property Chat                   $chat               Chat the user belongs to.
+ * @property User                   $from               Performer of the action, which resulted in the change.
+ * @property int                    $date               Date the change was done in Unix time.
+ * @property ChatMember             $old_chat_member    Previous information about the chat member.
+ * @property ChatMember             $new_chat_member    New information about the chat member.
+ * @property ChatInviteLink|null    $invite_link        (Optional). Chat invite link, which was used by the user to join the chat; for joining by invite link events only.
+ */
+class ChatMemberUpdated extends BaseObject
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function relations()
+    {
+        return [
+            'chat'            => Chat::class,
+            'from'            => User::class,
+            'old_chat_member' => ChatMember::class,
+            'new_chat_member' => ChatMember::class,
+            'invite_link'     => ChatInviteLink::class,
+        ];
+    }
+}
